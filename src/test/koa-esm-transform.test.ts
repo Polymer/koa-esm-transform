1ae0feb29c454927ce5e1a35b85ccf1e1caddf43
Brendan Baldwin <brendan@usergenic.com>
2019-08-20 12:52:45 -0700

    Make the esm transform tests a little more readable with the user agent detection.

--- src/test/koa-esm-transform.test.ts
+++ src/test/koa-esm-transform.test.ts
@@ -24,6 +24,15 @@ const transformRegenerator = require('@babel/plugin-transform-regenerator');
 const amdLoaderScript =
     readFileSync(require.resolve('@polymer/esm-amd-loader'), 'utf-8');
 
+const userAgents = {
+  chrome44:
+      'Mozilla/5.0 (Windows NT 6.2; WOW64) AppleWebKit/537.36 (KHTML like Gecko) Chrome/44.0.2403.155 Safari/537.36',
+  chrome70:
+      'Mozilla/5.0 (Linux; Android 9; SM-J500FN Build/PPR2.181005.003) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/70.0.3497.100 Safari/537.36',
+  opera48:
+      'Mozilla/5.0 (Linux; Android 9; SM-J500FN Build/PPR2.181005.003) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/69.0.3497.100 Mobile Safari/537.36 OPR/48.0.2310.131725',
+};
+
 test('inline AMD loader only when AMD transform used', async (t) => {
   t.plan(2);
   const logger = testLogger();
@@ -178,79 +187,90 @@ test('transform module scripts based on user agent', async (t) => {
         },
       },
       async (server) => {
-        const chrome44 =
-            'Mozilla/5.0 (Windows NT 6.2; WOW64) AppleWebKit/537.36 (KHTML like Gecko) Chrome/44.0.2403.155 Safari/537.36';
-        const myPageText44 = squeeze((await request(server)
-                                          .get('/my-page.html')
-                                          .set('User-Agent', chrome44))
-                                         .text);
-        const myPageText44IncludesTransformedScriptTags =
-            myPageText44.includes(squeeze(`
+        type TransformResult = {
+          pageText: string,
+          includesTransformedScriptTags?: boolean
+        };
+
+        /**
+         * Chrome 44 needs compilation of modules and ES2017 features
+         */
+        const chrome44: TransformResult = {
+          pageText: squeeze((await request(server)
+                                 .get('/my-page.html')
+                                 .set('User-Agent', userAgents.chrome44))
+                                .text)
+        };
+        chrome44
+            .includesTransformedScriptTags = chrome44.pageText.includes(squeeze(`
               <script>
                 define(['./x.js?${DEFAULT_QUERY_PARAM}'],
                   function (x) {"use strict"; x = _interopRequireWildcard(x);function _interopRequireWildcard(obj) {if (obj && obj.__esModule) {return obj;} else {var newObj = {};if (obj != null) {for (var key in obj) {if (Object.prototype.hasOwnProperty.call(obj, key)) {var desc = Object.defineProperty && Object.getOwnPropertyDescriptor ? Object.getOwnPropertyDescriptor(obj, key) : {};if (desc.get || desc.set) {Object.defineProperty(newObj, key, desc);} else {newObj[key] = obj[key];}}}}newObj.default = obj;return newObj;}}x();});
               </script><script>define(['./y.js?${
-                DEFAULT_QUERY_PARAM}'], function (_y) {"use strict";});</script><script>
+            DEFAULT_QUERY_PARAM}'], function (_y) {"use strict";});</script><script>
                 define([], function () { z();});
               </script>`));
-        if (!myPageText44IncludesTransformedScriptTags) {
+        if (!chrome44.includesTransformedScriptTags) {
           console.log(
               'Expected my-page.html to include transformed script tags:\n:',
-              myPageText44);
+              chrome44.pageText);
         }
         t.assert(
-            myPageText44IncludesTransformedScriptTags,
+            chrome44.includesTransformedScriptTags,
             'should transform the module content to AMD in .html files when modules unsupported (chrome 44)');
 
-
-        const opera48 =
-            'Mozilla/5.0 (Linux; Android 9; SM-J500FN Build/PPR2.181005.003) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/69.0.3497.100 Mobile Safari/537.36 OPR/48.0.2310.131725';
-        'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/65.0.3112.78 Safari/537.36';
-        const myPageText48 = squeeze((await request(server)
-                                          .get('/my-page.html')
-                                          .set('User-Agent', opera48))
-                                         .text);
-        const myPageText48IncludesTransformedScriptTags =
-            myPageText48.includes(squeeze(`
-              <script type="module">
-              import * as x from './x.js?${DEFAULT_QUERY_PARAM}';x();
-              </script>
-              <script type="module" src="./y.js?${
-                DEFAULT_QUERY_PARAM}"></script>
-              <script type="module">
-              z();
-              </script>`));
-        if (!myPageText48IncludesTransformedScriptTags) {
-          console.log(
-              'Expected my-page.html to include transformed script tags:\n',
-              myPageText48);
-        }
-        t.assert(
-            myPageText48IncludesTransformedScriptTags,
-            'should transform the module content in .html files when modules supported (opera 48)');
-
-
-        const chrome70 =
-            'Mozilla/5.0 (Linux; Android 9; SM-J500FN Build/PPR2.181005.003) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/70.0.3497.100 Safari/537.36';
-        const myPageText70 = squeeze((await request(server)
-                                          .get('/my-page.html')
-                                          .set('User-Agent', chrome70))
-                                         .text);
-        const myPageText70IncludesTransformedScriptTags =
-            myPageText70.includes(squeeze(`
+        /**
+         * Chrome 70 needs no transformation/compilation
+         */
+        const chrome70: TransformResult = {
+          pageText: squeeze((await request(server)
+                                 .get('/my-page.html')
+                                 .set('User-Agent', userAgents.chrome70))
+                                .text)
+        };
+        chrome70.includesTransformedScriptTags =
+            !chrome70.pageText.includes(squeeze(`
             <script type="module">
             import * as x from './x.js'; x();
             </script>
             <script type="module" src="./y.js"></script><script type="module">
             z();
             </script>`));
-        if (!myPageText70IncludesTransformedScriptTags) {
+        if (chrome70.includesTransformedScriptTags) {
           console.log(
               'Expected my-page.html to not have any transformations to module script tags:\n',
-              myPageText70);
+              chrome70.pageText);
         }
         t.assert(
-            myPageText70IncludesTransformedScriptTags,
+            !chrome70.includesTransformedScriptTags,
             'should not transform module content in .html files when no transforms needed (chrome 70)');
+
+        /**
+         * Opera has modules but needs some ES compilation, so query param
+         * should still be on module URLs
+         */
+        const opera48: TransformResult = {
+          pageText: squeeze((await request(server)
+                                 .get('/my-page.html')
+                                 .set('User-Agent', userAgents.opera48))
+                                .text)
+        };
+        opera48.includesTransformedScriptTags =
+            opera48.pageText.includes(squeeze(`
+                <script type="module">
+                import * as x from './x.js?${DEFAULT_QUERY_PARAM}';x();
+                </script><script type="module" src="./y.js?${
+                DEFAULT_QUERY_PARAM}"></script>
+                <script type="module">
+                z();
+                </script>`));
+        if (!opera48.includesTransformedScriptTags) {
+          console.log(
+              'Expected my-page.html to include transformed script tags:\n',
+              opera48.pageText);
+        }
+        t.assert(
+            opera48.includesTransformedScriptTags,
+            'should transform the module content in .html files when modules supported (opera 48)');
       });
 });
